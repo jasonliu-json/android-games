2e795bd0d82b0cf3dbc4fc42518a1ee053b8b3bc
Junyoung Seok <junyoung.seok@mail.utoronto.ca>
2019-10-29 16:59:36 -0400

    Added class TetrisGame to handle game logic and wrap classes Piece (+ subclasses) and Board.

---
+++ phase1/app/src/main/java/uoft/csc207/gameapplication/TetrisGame/TetrisGame.java
@@ -0,0 +1,77 @@
+package uoft.csc207.gameapplication.TetrisGame;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+/**
+ * IMPORTANT MESSAGE:
+ *
+ * Basically, all you have to do is create an instance of TetrisGame in TetrisGameDriver and hook
+ * up moveFallingPieceDown, moveFallingPieceLeft, moveFallingPieceRight to their respective
+ * user inputs in TetrisGameDriver.touchMove.
+ *
+ * This class, whether directly or indirectly, calculates everything regarding piece collision,
+ * clearing lines whenever necessary, moving fallingPiece down at constant time intervals, etc.
+ *
+ * The only information you need to draw the game onto the screen is contained in the attribute
+ * board, which you can call using getBoard (I think this should be done in the
+ * TetrisGameDriver.draw method, which is called every 30 ms).
+ *
+ * All you have to do is parse this 2D array and draw the shapes based on the entries, i.e. '.' is
+ * empty, 'I' should be a cyan tile, 'O' should be a yellow tile, etc.
+ *
+ */
+class TetrisGame {
+
+    private Piece fallingPiece;
+
+    private BoardV2 board;
+    private Randomizer pieceGenerator;
+
+    private Timer timer;
+
+    private int score;   // not used at the moment
+
+    TetrisGame() {
+        board = new BoardV2();
+        pieceGenerator = new Randomizer();
+        fallingPiece = pieceGenerator.nextPiece();
+
+        // setup timer
+        timer = new Timer();
+        TimerTask makePieceFall = new TimerTask() {
+            @Override
+            public void run() {
+                if (!board.clearRows()) {
+                    TetrisGame.this.moveFallingPieceDown();
+                }
+            }
+        };
+        timer.schedule(makePieceFall, 500, 500);   // piece falls every 500 ms
+    }
+
+    public BoardV2 getBoard() {
+        return board;
+    }
+
+    void moveFallingPieceDown() {
+        board.removePiece(fallingPiece);
+        if (!fallingPiece.moveDown(board)) {   // cannot move down
+            board.addPiece(fallingPiece);
+            fallingPiece = pieceGenerator.nextPiece();
+        }
+        board.addPiece(fallingPiece);
+    }
+
+    void moveFallingPieceLeft() {
+        board.removePiece(fallingPiece);
+        fallingPiece.moveLeft(board);
+        board.addPiece(fallingPiece);
+    }
+
+    void moveFallingPieceRight() {
+        board.removePiece(fallingPiece);
+        fallingPiece.moveRight(board);
+        board.addPiece(fallingPiece);
+    }
+}
