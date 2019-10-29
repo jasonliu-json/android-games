72b8ad1039ba215de4cbda6b294676cc92813fff
xiaguan2 <xiaguangwei@Isaacs-MacBook-Pro.local>
2019-10-29 04:11:00 -0400

    I just add the draw method for the Board class

--- phase1/app/src/main/java/uoft/csc207/gameapplication/TetrisGame/BoardV2.java
+++ phase1/app/src/main/java/uoft/csc207/gameapplication/TetrisGame/BoardV2.java
@@ -1,6 +1,11 @@
 package uoft.csc207.gameapplication.TetrisGame;
 
 import java.util.Arrays;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 
 class BoardV2 {
 
@@ -65,6 +70,27 @@ class BoardV2 {
             }
         }
         return rowsCleared;
+    }
+    public void drawBoard(Canvas canvas) {
+        float width = canvas.getWidth() / 12;
+        float height = canvas.getWidth() / 12;
+        int i, j;
+        float x, y;
+
+        x = 0;
+        y = 0;
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.BLACK);
+
+        for (i = 1; i <= 11; i++) {
+            canvas.drawLine(x, 0, x, width * 20, paint);
+            x = x + width;
+        }
+        for (j = 1; j <= 21; j++) {
+            canvas.drawLine(0, y, height * 10, y, paint);
+            y = y + height;
+        }
     }
 }
 
