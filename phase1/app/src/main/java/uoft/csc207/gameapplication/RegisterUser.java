2ea6c7369d373eec92ec3600a2cd095ed16e1c34
pengtin3 <pengray4@gmail.com>
2019-10-30 03:14:04 -0400

    Added same load method as login

--- phase1/app/src/main/java/uoft/csc207/gameapplication/RegisterUser.java
+++ phase1/app/src/main/java/uoft/csc207/gameapplication/RegisterUser.java
@@ -10,12 +10,24 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class RegisterUser extends AppCompatActivity {
     private Button registerButton;
     private EditText emailInput;
     private EditText usernameInput;
     private EditText passwordInput;
     private EditText passwordConfirmationInput;
+
+    private static final String FILE = "UserData.json";
+    private JSONObject jsonObject;
 
     String registerEmail;
     String registerUsername;
@@ -31,6 +43,7 @@ public class RegisterUser extends AppCompatActivity {
         usernameInput = (EditText) findViewById(R.id.register_username);
         passwordInput = (EditText) findViewById(R.id.register_password);
         passwordConfirmationInput = (EditText) findViewById(R.id.register_password_confirmation);
+        load();
 
 
 
@@ -53,6 +66,36 @@ public class RegisterUser extends AppCompatActivity {
                 }
             }
         });
+    }
+    
+    private void load() {
+        try {
+            int i = 0;
+            String jsonString = "";
+            try {
+                FileInputStream fileInputStream = openFileInput(FILE);
+                while ((i = fileInputStream.read()) != -1) {
+                    jsonString += (char) i;
+                }
+            }
+            catch (FileNotFoundException e) {
+                BufferedReader bufferReader = new BufferedReader(
+                        new InputStreamReader(getAssets().open(FILE)));
+                while ((i = bufferReader.read()) != -1) {
+                    jsonString += (char) i;
+                }
+            }
+            jsonObject = new JSONObject(jsonString);
+        }
+        catch (FileNotFoundException e) {
+            e.printStackTrace();;
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void showToast(String text) {
