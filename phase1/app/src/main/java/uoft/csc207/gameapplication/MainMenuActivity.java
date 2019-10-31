f4e49720a0bba70a0048700d087b750127de91e8
liumin52 <mx.liu@mail.utoronto.ca>
2019-10-31 15:20:04 -0400

    added MainMenuActivity - need to implement two more buttons

---
+++ phase1/app/src/main/java/uoft/csc207/gameapplication/MainMenuActivity.java
@@ -0,0 +1,48 @@
+package uoft.csc207.gameapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    private Button playButton;
+    private Button leaderboardButton;
+    private Button scoreButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu);
+        playButton = (Button) findViewById(R.id.play_button);
+        leaderboardButton = (Button) findViewById(R.id.leaderboard_button);
+        scoreButton = (Button) findViewById(R.id.score_button);
+
+        playButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gameActivity = new Intent(MainMenuActivity.this, GameActivity.class);
+                startActivity(gameActivity);
+            }
+
+        });
+    }
+
+
+//
+//        registerText.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent registerUser = new Intent(Login.this, RegisterUser.class);
+//                startActivity(registerUser);
+//            }
+//        });
+//    }
+
+
+}
