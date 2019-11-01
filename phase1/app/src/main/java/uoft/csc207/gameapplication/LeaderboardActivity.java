da5d5de8c7344528994bb3a11b4270757f7ba621
liumin52 <mx.liu@mail.utoronto.ca>
2019-10-31 22:17:57 -0400

    sorry same as last commit

--- phase1/app/src/main/java/uoft/csc207/gameapplication/LeaderboardActivity.java
+++ phase1/app/src/main/java/uoft/csc207/gameapplication/LeaderboardActivity.java
@@ -5,40 +5,53 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.lang.reflect.Array;
 
 public class LeaderboardActivity extends AppCompatActivity {
 
     private String[] topTenPlayers = new String[10];
     private String[] topTenScores = new String[10];
-    private String leaderboard = "Player          Score \n";
+    private TextView[] rows = new TextView[10];
+    private TextView titleRow;
+
+    public static String fixedLengthString(String string, int length) {
+        return String.format("%1$"+length+ "s", string);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leaderboard);
 
-        topTenPlayers = new String[] {"p1", "p2", "p3", "p4", "p5", "p6", "p7787", "p888", "p9", "p10"};
+        topTenPlayers = new String[] {"p1", "p2", "p3", "p4", "p5", "p6", "p7", "p8", "p9", "p10"};
         topTenScores = new String[] {"10", "9", "8", "7", "6", "5", "4", "3", "2", "1"};
-        for (int i=0; i<10; i++) {
-            String row = topTenPlayers[i];
-            while (row.length() < 16) {
-                row += " ";
-            }
 
-            while (row.length() < 20 - topTenScores[i].length()) {
-                row += " ";
-            }
-            row += topTenScores[i];
-            leaderboard += row + " \n";
+        titleRow = findViewById(R.id.leaderboard_title);
+
+        rows[0] = (TextView) findViewById(R.id.row1);
+        rows[1] = (TextView) findViewById(R.id.row2);
+        rows[2] = (TextView) findViewById(R.id.row3);
+        rows[3] = (TextView) findViewById(R.id.row4);
+        rows[4] = (TextView) findViewById(R.id.row5);
+        rows[5] = (TextView) findViewById(R.id.row6);
+        rows[6] = (TextView) findViewById(R.id.row7);
+        rows[7] = (TextView) findViewById(R.id.row8);
+        rows[8] = (TextView) findViewById(R.id.row9);
+        rows[9] = (TextView) findViewById(R.id.row10);
+
+        titleRow.setText("Player          Score");
+        for (int i=0; i<10; i++) {
+
+            String player = String.format("%1$"+ (-15) +"s", topTenPlayers[i]).replace(' ', '_');
+            String score = String.format("%1$"+5+"s", topTenScores[i]).replace(' ', '_');
+            String row = player + score;
+            rows[i].setText(row);
+//      System.out.println(row);
         }
 
-        TextView textView = (TextView) findViewById(R.id.leaderboard_table);
-        textView.setTextSize(20);
-        textView.setText(leaderboard);
-        // max characters in line is weird, like 7
-
-//    System.out.println(leaderboard);
     }
 }
 
