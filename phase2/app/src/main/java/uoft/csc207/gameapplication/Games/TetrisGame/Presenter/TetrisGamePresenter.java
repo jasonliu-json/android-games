b0d2dcbbe6ed7474f741d47660e27350a4b15d09
Junyoung Seok <junyoung.seok@mail.utoronto.ca>
2019-12-01 03:27:07 -0500

    Added javadoc for TetrisGameController. Changed variable names in TetrisGamePresenter.

--- phase2/app/src/main/java/uoft/csc207/gameapplication/Games/TetrisGame/Presenter/TetrisGamePresenter.java
+++ phase2/app/src/main/java/uoft/csc207/gameapplication/Games/TetrisGame/Presenter/TetrisGamePresenter.java
@@ -20,7 +20,7 @@ public class TetrisGamePresenter {
     private Map<String, Integer> colorScheme;
 
     /**
-     * A map between the colors defined in the color scheme to the specifc blocks in Tetris.
+     * A map for the colors held in the color scheme to their corresponding Tetris blocks.
      */
     private Map<Character, String> pieceToColor;
 
@@ -31,7 +31,6 @@ public class TetrisGamePresenter {
      */
     public TetrisGamePresenter(Map<String, Integer> colorScheme) {
         this.colorScheme = colorScheme;
-
         pieceToColor = new HashMap<>();
         pieceToColor.put('I', "BlockColour1");
         pieceToColor.put('J', "BlockColour2");
@@ -66,20 +65,20 @@ public class TetrisGamePresenter {
      * @param grid The 2D array representation of a board to be displayed.
      */
     private void drawGrid(Canvas canvas, char[][] grid) {
-        float length = canvas.getWidth() / (grid[0].length + 2);
-        float x = 0;
-        float y = 0;
+        int unit = canvas.getWidth() / (grid[0].length + 2);  // length of one unit on the grid
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.FILL);
         paint.setColor(Color.LTGRAY);
         paint.setStrokeWidth(3);
-        for (int i = 0; i < 11; i++) {
-            canvas.drawLine(x, 0, x, length * 20, paint);
-            x += length;
+        int x = 0;
+        for (int i = 0; i < grid[0].length + 1; i++) {
+            canvas.drawLine(x, 0, x, unit * grid.length, paint);
+            x += unit;
         }
-        for (int j = 0; j < 21; j++) {
-            canvas.drawLine(0, y, length * 10, y, paint);
-            y += length;
+        int y = 0;
+        for (int j = 0; j < grid.length + 1; j++) {
+            canvas.drawLine(0, y, unit * grid[0].length, y, paint);
+            y += unit;
         }
     }
 
@@ -90,20 +89,21 @@ public class TetrisGamePresenter {
      * @param grid The 2D array representation of a board to be displayed.
      */
     private void drawBlocks(Canvas canvas, char[][] grid) {
+        int unit = canvas.getWidth() / (grid[0].length + 2);  // length of one unit on the grid
         Paint paint = new Paint();
         paint.setStyle(Paint.Style.FILL);
-        int length = canvas.getWidth() / 12;
-        for (int i = 0; i < grid.length; i++) {
-            for (int k = 0; k < grid[0].length; k++) {
-                char block = grid[i][k];
-                int x = k * length;
-                int y = i * length;
+        for (int y = 0; y < grid.length; y++) {
+            for (int x = 0; x < grid[0].length; x++) {
+                char block = grid[y][x];
+                int xRect = x * unit;
+                int yRect = y * unit;
                 if (block != '.') {
                     Integer color = colorScheme.get(pieceToColor.get(block));
                     if (color != null) {
                         paint.setColor(color);
                     }
-                    canvas.drawRect(new Rect(x, y, x + length, y + length), paint);
+                    Rect rect = new Rect(xRect, yRect, xRect + unit, yRect + unit);
+                    canvas.drawRect(rect, paint);
                 }
             }
         }
